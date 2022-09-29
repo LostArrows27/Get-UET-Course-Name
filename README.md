af66d3388a8c50bdcfff1259f6b1146640857030
Nguyễn Thành Dũng <thelastofus2isnuts@gmail.com>
2022-09-29 23:02:18 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Get-UET-Course-Name
+ Since all course's name in UET Course page is all sign and number so yeah:D
---
+++ changeName.js
@@ -0,0 +1,21 @@
+var UETNavigation = document.querySelector('.block_navigation');
+
+if (UETNavigation) {
+    var courseName = [];
+    var courseIdList = UETNavigation.querySelectorAll('li.type_course.depth_3')
+    for (let i = 0; i < courseIdList.length; i++) {
+        var currentCourseInfo = courseIdList[i].innerText;
+        var currentCourseId = currentCourseInfo.split('_')[1];
+        var currentCourseClass = currentCourseInfo.split('_')[2];
+        var currentCourseName = courseIdList[i].querySelector('a');
+        var findClass = currentCourseName.title.split('(');
+        currentCourseName.innerText = findClass[0] 
+        if(currentCourseId && currentCourseClass) {
+            if(currentCourseClass.length > 5) {
+                currentCourseClass = currentCourseClass.slice(0, 2);
+            }
+            currentCourseName.innerText += `\n(${currentCourseId} ${currentCourseClass})`;
+        } 
+
+    }
+}
